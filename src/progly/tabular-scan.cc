f65a506fe4082228d33fe590c76d40bab60d411b
Noah Watkins <noahwatkins@gmail.com>
2016-11-30 13:32:51 -0800

    progly: add cls_tabular with selectivity filtering
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/progly/tabular-scan.cc
+++ src/progly/tabular-scan.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <boost/program_options.hpp>
 #include "include/rados/librados.hpp"
+#include "cls/tabular/cls_tabular.h"
 
 namespace po = boost::program_options;
 
@@ -18,6 +19,8 @@ int main(int argc, char **argv)
   unsigned rows_per_obj;
   double selectivity;
   std::string pool;
+  bool use_cls;
+  bool use_index;
 
   po::options_description gen_opts("General options");
   gen_opts.add_options()
@@ -26,6 +29,8 @@ int main(int argc, char **argv)
     ("num-rows", po::value<unsigned>(&num_rows)->required(), "number of rows")
     ("rows-per-obj", po::value<unsigned>(&rows_per_obj)->required(), "rows per object")
     ("selectivity", po::value<double>(&selectivity)->required(), "selectivity pct")
+    ("use-cls", po::bool_switch(&use_cls)->default_value(false), "filter in cls")
+    ("use-index", po::bool_switch(&use_index)->default_value(false), "use cls index")
 		("pool,p", po::value<std::string>(&pool)->required(), "pool")
   ;
 
@@ -51,6 +56,9 @@ int main(int argc, char **argv)
   assert(selectivity <= 100.0);
   selectivity /= 100.0;
 
+  if (use_index)
+    assert(use_cls);
+
   // connect to rados
   librados::Rados cluster;
   cluster.init(NULL);
@@ -65,7 +73,6 @@ int main(int argc, char **argv)
 
   uint64_t max_val = range_size * selectivity;
 
-  uint64_t total_rows = 0;
   uint64_t filtered_rows = 0;
 
   const unsigned num_objs = num_rows / rows_per_obj;
@@ -75,24 +82,40 @@ int main(int argc, char **argv)
     const std::string oid = ss.str();
 
     ceph::bufferlist bl;
-    int ret = ioctx.read(oid, bl, 0, 0);
-    assert(ret > 0);
 
-    assert(bl.length() / sizeof(uint64_t) == rows_per_obj);
+    if (use_cls) {
+      scan_op op;
+      op.max_val = max_val;
+      op.use_index = use_index;
+      ceph::bufferlist inbl;
+      ::encode(op, inbl);
+      int ret = ioctx.exec(oid, "tabular", "scan", inbl, bl);
+      checkret(ret, 0);
+    } else {
+      int ret = ioctx.read(oid, bl, 0, 0);
+      assert(ret > 0);
+      assert(bl.length() > 0);
+      assert(bl.length() / sizeof(uint64_t) == rows_per_obj);
+    }
+
+    const uint64_t row_count = bl.length() / sizeof(uint64_t);
     const uint64_t *rows = (uint64_t*)bl.c_str();
-    for (unsigned r = 0; r < rows_per_obj; r++) {
+
+    for (unsigned r = 0; r < row_count; r++) {
       const uint64_t row_val = rows[r];
-      if (row_val < max_val)
+      if (use_cls) {
+        assert(row_val < max_val);
         filtered_rows++;
-      total_rows++;
+      } else if (row_val < max_val)
+        filtered_rows++;
     }
   }
 
-  std::cout << "total rows " << total_rows
+  std::cout << "total rows " << num_rows
     << " filtered rows " << filtered_rows
     << " selectivity wanted " << (100.0 * selectivity)
     << " selectivity observed "
-    << (100.0 * (double)filtered_rows / (double)total_rows)
+    << (100.0 * (double)filtered_rows / (double)num_rows)
     << std::endl;
 
   ioctx.close();
