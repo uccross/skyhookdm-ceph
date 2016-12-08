12671ea9f5c52d8e118a15e1c66cf24aba1637cd
Noah Watkins <noahwatkins@gmail.com>
2016-12-08 12:44:33 -0800

    progly: calculate index on write
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/cls/tabular/cls_tabular.cc
+++ src/cls/tabular/cls_tabular.cc
@@ -10,6 +10,7 @@ CLS_NAME(tabular)
 
 cls_handle_t h_class;
 cls_method_handle_t h_scan;
+cls_method_handle_t h_add;
 
 struct tab_index {
   uint64_t min;
@@ -33,6 +34,42 @@ struct tab_index {
 };
 WRITE_CLASS_ENCODER(tab_index)
 
+static int add(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
+{
+  const uint64_t *rows = (uint64_t*)in->c_str();
+  const uint64_t row_count = in->length() / sizeof(uint64_t);
+
+  if (row_count == 0) {
+    CLS_ERR("NO ROWS");
+    return -EINVAL;
+  }
+
+  struct tab_index idx;
+
+  idx.min = idx.max = rows[0];
+  for (unsigned i = 0; i < row_count; i++) {
+    const uint64_t row_val = rows[i];
+    idx.min = std::min(row_val, idx.min);
+    idx.max = std::max(row_val, idx.max);
+  }
+
+  bufferlist hdrbl;
+  ::encode(idx, hdrbl);
+  int ret = cls_cxx_map_write_header(hctx, &hdrbl);
+  if (ret < 0) {
+    CLS_ERR("ERROR: writing obj hdr %d", ret);
+    return ret;
+  }
+
+  ret = cls_cxx_write_full(hctx, in);
+  if (ret < 0) {
+    CLS_ERR("ERROR: writing obj full %d", ret);
+    return ret;
+  }
+
+  return 0;
+}
+
 static int scan(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
 {
   scan_op op;
@@ -46,7 +83,6 @@ static int scan(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
   }
 
   struct tab_index idx;
-  bool has_index = false;
 
   if (op.use_index) {
     // read index header
@@ -57,19 +93,21 @@ static int scan(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
       return ret;
     }
 
-    if (hdrbl.length() > 0) {
-      try {
-        bufferlist::iterator it = hdrbl.begin();
-        ::decode(idx, it);
-        has_index = true;
-      } catch (const buffer::error &err) {
-        CLS_ERR("ERROR: decoding index");
-        return -EIO;
-      }
-
-      if (op.max_val < idx.min)
-        return 0;
+    if (hdrbl.length() == 0) {
+      CLS_ERR("ERROR: no header");
+      return -EINVAL;
     }
+
+    try {
+      bufferlist::iterator it = hdrbl.begin();
+      ::decode(idx, it);
+    } catch (const buffer::error &err) {
+      CLS_ERR("ERROR: decoding index");
+      return -EIO;
+    }
+
+    if (op.max_val < idx.min)
+      return 0;
   }
 
   bufferlist bl;
@@ -82,32 +120,10 @@ static int scan(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
   const uint64_t *rows = (uint64_t*)bl.c_str();
   const uint64_t row_count = bl.length() / sizeof(uint64_t);
 
-  // build the index?
-  if (op.use_index && !has_index) {
-    if (row_count > 0) {
-      idx.min = idx.max = rows[0];
-      for (unsigned i = 0; i < row_count; i++) {
-        const uint64_t row_val = rows[i];
-        idx.min = std::min(row_val, idx.min);
-        idx.max = std::max(row_val, idx.max);
-        if (row_val < op.max_val)
-          out->append((char*)&row_val, sizeof(row_val));
-      }
-
-      bufferlist hdrbl;
-      ::encode(idx, hdrbl);
-      ret = cls_cxx_map_write_header(hctx, &hdrbl);
-      if (ret < 0) {
-        CLS_ERR("ERROR: writing obj hdr %d", ret);
-        return ret;
-      }
-    }
-  } else {
-    for (unsigned i = 0; i < row_count; i++) {
-      const uint64_t row_val = rows[i];
-      if (row_val < op.max_val)
-        out->append((char*)&row_val, sizeof(row_val));
-    }
+  for (unsigned i = 0; i < row_count; i++) {
+    const uint64_t row_val = rows[i];
+    if (row_val < op.max_val)
+      out->append((char*)&row_val, sizeof(row_val));
   }
 
   return 0;
@@ -121,4 +137,7 @@ void __cls_init()
 
   cls_register_cxx_method(h_class, "scan",
       CLS_METHOD_RD | CLS_METHOD_WR, scan, &h_scan);
+
+  cls_register_cxx_method(h_class, "add",
+      CLS_METHOD_RD | CLS_METHOD_WR, add, &h_add);
 }
