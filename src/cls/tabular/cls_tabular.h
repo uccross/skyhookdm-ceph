b7d94c8eb5c66a7f98b9b77340658469a3a0cedd
Noah Watkins <noahwatkins@gmail.com>
2017-03-04 13:44:41 -0800

    progly: add regex scan example
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/cls/tabular/cls_tabular.h
+++ src/cls/tabular/cls_tabular.h
@@ -30,4 +30,32 @@ struct scan_op {
 };
 WRITE_CLASS_ENCODER(scan_op)
 
+struct regex_scan_op {
+  size_t row_size;
+  size_t field_offset;
+  size_t field_length;
+  std::string regex;
+
+  regex_scan_op() {}
+
+  void encode(bufferlist& bl) const {
+    ENCODE_START(1, 1, bl);
+    ::encode(row_size, bl);
+    ::encode(field_offset, bl);
+    ::encode(field_length, bl);
+    ::encode(regex, bl);
+    ENCODE_FINISH(bl);
+  }
+
+  void decode(bufferlist::iterator& bl) {
+    DECODE_START(1, bl);
+    ::decode(row_size, bl);
+    ::decode(field_offset, bl);
+    ::decode(field_length, bl);
+    ::decode(regex, bl);
+    DECODE_FINISH(bl);
+  }
+};
+WRITE_CLASS_ENCODER(regex_scan_op)
+
 #endif
