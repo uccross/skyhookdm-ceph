5b0d8ff86e1aea2e81aa1380b1de692c1dcdba7d
nwatkins <nwatkins@client0.nwatkins-qv23275.zlog-pg0.clemson.cloudlab.us>
2017-03-22 14:19:58 -0400

    progly: parallel rados import
    Signed-off-by: nwatkins <nwatkins@client0.nwatkins-qv23275.zlog-pg0.clemson.cloudlab.us>

--- src/progly/rados-store-glob.sh
+++ src/progly/rados-store-glob.sh
@@ -7,6 +7,8 @@ if [[ $# < 1 ]]; then
   exit 1
 fi
 
+groupsize=5
+
 pool=$1
 shift
 objects=($@)
@@ -16,11 +18,15 @@ echo ${objects[@]} | fold  -w 80 -s
 read -p "Continue? (y/n) " -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   count=0
-  for objfile in ${objects[@]}; do
-    objname="obj.${count}"
-    rados -p $pool rm $objname || true
-    echo "writing $objfile into $pool/$objname"
-    rados -p $pool put $objname $objfile
-    count=$((count+1))
+  for ((i=0; i < ${#objects[@]}; i+=groupsize)); do
+    objgroup=("${objects[@]:i:groupsize}")
+    for objfile in ${objgroup[*]}; do
+      objname="obj.${count}"
+      rados -p $pool rm $objname || true
+      echo "writing $objfile into $pool/$objname"
+      rados -p $pool put $objname $objfile &
+      count=$((count+1))
+    done
+    wait
   done
 fi
