38fede1af5551f3ad5a3ed4324f1c087bbda9a12
nwatkins <nwatkins@client0.nwatkins-qv23275.zlog-pg0.clemson.cloudlab.us>
2017-03-22 18:07:05 -0400

    progly: partial results
    Signed-off-by: nwatkins <nwatkins@client0.nwatkins-qv23275.zlog-pg0.clemson.cloudlab.us>

--- src/progly/cloudlab/all.sh
+++ src/progly/cloudlab/all.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-pools="tpc2osd tpc2osd"
-nobjs=150
+pools="tpc1osd tpc2osd"
+nobjs=10001
 
 for pool in $pools; do
   for script in qa qb qc qd qe qf; do
-    ./${script}.sh $pool $nobjs 36
-    ./${script}.sh $pool $nobjs 36 --use-cls
+    ./${script}.sh $pool $nobjs 40
+    ./${script}.sh $pool $nobjs 40 --use-cls
   done
-  ./qd-index.sh $pool $nobjs 36
-  ./qf-proj.sh $pool $nobjs 36
+  ./qd-index.sh $pool $nobjs 40
+  ./qf-proj.sh $pool $nobjs 40
 done
