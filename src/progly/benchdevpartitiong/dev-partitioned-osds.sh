690e513a14a2c81a2edadd6941782eb27932b404
jlefevre <jlefevre@client0.osd-slicing-7.skyhook-pg0.wisc.cloudlab.us>
2017-04-13 23:09:10 -0600

    1osd scaling num objects

--- src/progly/benchdevpartitiong/dev-partitioned-osds.sh
+++ src/progly/benchdevpartitiong/dev-partitioned-osds.sh
@@ -6,9 +6,9 @@ echo `date`
 
 runquery="/usr/bin/run-query"
 nosds=$1
-nobjs=10000
+objs="5000 7500 10000"
 pool=tpc
-nthreads="20 40 60 80"
+nthreads="20"
 runs="1 2 3 4"
 extendedprice=71000
 selectivity=10
@@ -16,27 +16,29 @@ echo "nosds="$nosds
 
 function executequery() {
   local cmd="$runquery $1"
-  for rr in $runs; do
-    cachetype="hot"
-    echo "todo next:" $1
-    if [ $rr -lt 3 ]; then
+    for rr in $runs; do
+      cachetype="hot"
+      if [ $rr -lt 3 ]; then
         cachetype="cold"
         echo "clearing cache on osd"0
         ssh osd0 sync
         ssh osd0 "echo 1 | sudo tee /proc/sys/vm/drop_caches"
-    fi
-    start=$(date --utc "+%s.%N")
-    $cmd
-    end=$(date --utc "+%s.%N")
-    dur=0$(echo "$end - $start" | bc)
-    echo $cmd cache=$cachetype run-${rr} $dur
-  done
+      fi
+      start=$(date --utc "+%s.%N")
+      $cmd
+      end=$(date --utc "+%s.%N")
+      dur=0$(echo "$end - $start" | bc)
+      echo $cmd cache=$cachetype start=$start end=$end run-${rr} $dur
+      sleep 10
+    done
 }
 
 
 for nthread in ${nthreads}; do
+  for nobjs in $objs; do
     args=" --query b --num-objs $nobjs --pool $pool --nthreads $nthread --quiet --extended-price ${extendedprice}"
     executequery "$args "
     executequery "$args --use-cls"
+  done
 done
 echo `date`
