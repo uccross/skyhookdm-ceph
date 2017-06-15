cc9b7efe3eca541c34a21ef47094493ab481ba85
jlefevre <jlefevre@client0.split-querying.skyhook-pg0.wisc.cloudlab.us>
2017-06-15 18:31:15 -0500

    query b over static set of objs after progressively splitting to objs.

---
+++ src/progly/cloudlab_split_objs_querying/4osds-split-objs-querying.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+#set -x
+echo "./this <nosds> <nobjs>"
+
+echo START at `date`
+
+nosds=$1
+queue_depth=$((${nosds}*12))
+nobjs=$2
+worker_threads=10
+pool=tpc
+
+q_ab_extended_prices=(71000.0)
+q_ab_selectivities=(10)
+
+function run_query() {
+    local cmd=$1
+    selectivity=$2
+    echo "begin function run_query with selectivity percent=" $2
+    for r in `seq 1 3`; do
+        echo "starting run"$r
+        cachetype="cold"
+        for ((j=0; j<${nosds}; j++)); do
+            echo "clearing cache on osd"$j
+            ssh osd${j} sync
+            ssh osd${j} "echo 1 | sudo tee /proc/sys/vm/drop_caches"
+            ssh osd${j} sync
+        done
+        start=$(date --utc "+%s.%N")
+        $cmd
+        end=$(date --utc "+%s.%N")
+        dur=0$(echo "$end - $start" | bc)      
+        echo $cmd selectivity-pct=$selectivity cache=$cachetype start=$start end=$end run-${r} $dur
+        sleep 5
+    done
+}
+
+
+cmdbase="run-query --num-objs ${nobjs} --pool ${pool} --wthreads ${worker_threads} --qdepth ${queue_depth} --quiet"
+
+# setup for queries: a, b
+for ((i=0; i<${#q_ab_extended_prices[@]}; i++)); do
+    price=${q_ab_extended_prices[i]}
+    selpct=${q_ab_selectivities[i]}
+    cmd="${cmdbase} --extended-price ${price}"
+    for qname in b; do
+        run_query "$cmd --query ${qname}" "${selpct}"
+        run_query "$cmd --query ${qname} --use-cls" "${selpct}"
+    done
+done
+
+echo END at `date`
+
