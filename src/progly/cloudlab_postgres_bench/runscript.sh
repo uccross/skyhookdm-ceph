5db1f572bde2e9c5f628935f1d4c17a9234df387
PostgreSQL administrator <postgres@client0.pgtest2.skyhook-pg0.wisc.cloudlab.us>
2017-09-15 15:32:51 -0500

    readme file to run the experiment, and slight change to runscript.sh.

--- src/progly/cloudlab_postgres_bench/runscript.sh
+++ src/progly/cloudlab_postgres_bench/runscript.sh
@@ -2,7 +2,7 @@
 set -e
 
 date
-t=lineitem1bssd
+t=$1
 for q in qa.10pct.int.sql  qa.10pct.sql qb.10pct.1col.sql qb.10pct.3cols.sql qb.10pct.sql qd.sql qf.10pct.1col.sql qf.10pct.count.sql qf.10pct.sql; do
     ./pg-bench.sh $t $q | tee -a pgresult-$t-$q.log
 done
