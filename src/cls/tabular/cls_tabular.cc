d9b75319cb26330ae319bbd6d8eeae5c996d13f1
Jeff LeFevre <jlefevre@cs.ucsc.edu>
2018-07-20 15:00:12 -0700

    removed older unused code from cls_tabular and added some comments.

--- src/cls/tabular/cls_tabular.cc
+++ src/cls/tabular/cls_tabular.cc
@@ -23,13 +23,9 @@ CLS_VER(1,0)
 CLS_NAME(tabular)
 
 cls_handle_t h_class;
-cls_method_handle_t h_scan;
-cls_method_handle_t h_read;
-cls_method_handle_t h_add;
-cls_method_handle_t h_regex_scan;
 cls_method_handle_t h_query_op;
 cls_method_handle_t h_build_index;
-cls_method_handle_t h_test_par;
+
 
 static inline uint64_t __getns(clockid_t clock)
 {
@@ -44,93 +40,15 @@ static inline uint64_t getns()
   return __getns(CLOCK_MONOTONIC);
 }
 
-struct tab_index {
-  uint64_t min;
-  uint64_t max;
-
-  tab_index() {}
-
-  void encode(bufferlist& bl) const {
-    ENCODE_START(1, 1, bl);
-    ::encode(min, bl);
-    ::encode(max, bl);
-    ENCODE_FINISH(bl);
-  }
-
-  void decode(bufferlist::iterator& bl) {
-    DECODE_START(1, bl);
-    ::decode(min, bl);
-    ::decode(max, bl);
-    DECODE_FINISH(bl);
-  }
-};
-WRITE_CLASS_ENCODER(tab_index)
-
+// extract bytes as string for regex matching
 static std::string string_ncopy(const char* buffer, std::size_t buffer_size) {
   const char* copyupto = std::find(buffer, buffer + buffer_size, 0);
   return std::string(buffer, copyupto);
 }
 
-static int add(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
-{
-  const uint64_t *rows = (uint64_t*)in->c_str();
-  const uint64_t row_count = in->length() / sizeof(uint64_t);
-
-  if (row_count == 0) {
-    CLS_ERR("NO ROWS");
-    return -EINVAL;
-  }
-
-  struct tab_index idx;
-
-  idx.min = idx.max = rows[0];
-  for (unsigned i = 0; i < row_count; i++) {
-    const uint64_t row_val = rows[i];
-    idx.min = std::min(row_val, idx.min);
-    idx.max = std::max(row_val, idx.max);
-  }
-
-#if 0
-  bufferlist hdrbl;
-  ::encode(idx, hdrbl);
-  int ret = cls_cxx_map_write_header(hctx, &hdrbl);
-  if (ret < 0) {
-    CLS_ERR("ERROR: writing obj hdr %d", ret);
-    return ret;
-  }
-#endif
-
-  bufferlist attr_bl;
-  ::encode(idx.min, attr_bl);
-  int ret = cls_cxx_setxattr(hctx, "min", &attr_bl);
-  if (ret < 0) {
-    CLS_ERR("ERROR: writing min xattr %d", ret);
-    return ret;
-  }
-
-  ret = cls_cxx_write_full(hctx, in);
-  if (ret < 0) {
-    CLS_ERR("ERROR: writing obj full %d", ret);
-    return ret;
-  }
-
-  return 0;
-}
-
-static int read(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
-{
-  bufferlist bl;
-  int ret = cls_cxx_read(hctx, 0, 0, &bl);
-  if (ret < 0) {
-    CLS_ERR("ERROR: reading obj %d", ret);
-    return ret;
-  }
-
-  ::encode(bl, *out);
-
-  return 0;
-}
-
+/*
+ * Convert integerto string for index/omap of primary key
+ */
 static inline std::string u64tostr(uint64_t value)
 {
   std::stringstream ss;
@@ -156,6 +74,11 @@ static inline int strtou64(const std::string value, uint64_t *out)
   return 0;
 }
 
+
+/*
+ * Build an index from the primary key (orderkey,linenum), insert to omap.
+ * Index contains <k=primarykey, v=offset of row within BL>
+ */
 static int build_index(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
 {
   uint32_t batch_size;
@@ -184,7 +107,7 @@ static int build_index(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
 
 
   std::map<string, bufferlist> index;
-
+  // read all rows and extract the key fields
   for (size_t rid = 0; rid < num_rows; rid++) {
     const char *row = rows + rid * row_size;
     const char *o_vptr = row + order_key_field_offset;
@@ -228,38 +151,7 @@ static int build_index(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
   return 0;
 }
 
-static int test_par(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
-{
-  uint64_t iters;
-  bool readobj;
 
-  try {
-    bufferlist::iterator it = in->begin();
-    ::decode(iters, it);
-    ::decode(readobj, it);
-  } catch (const buffer::error &err) {
-    CLS_ERR("ERROR: decoding iters");
-    return -EINVAL;
-  }
-
-  if (readobj) {
-    bufferlist bl;
-    int ret = cls_cxx_read(hctx, 0, 0, &bl);
-    if (ret < 0) {
-      CLS_ERR("ERROR: reading obj %d", ret);
-      return ret;
-    }
-  }
-
-  int sum = 0;
-  iters = std::min(iters, (uint64_t)10000000000ULL);
-  for (uint64_t count = 0; count < iters; count++)
-    sum += (int)count;
-
-  ::encode(sum, *out);
-
-  return 0;
-}
 
 // busy loop work
 volatile uint64_t __tabular_x;
@@ -270,10 +162,13 @@ static void add_extra_row_cost(uint64_t cost)
   }
 }
 
+/*
+ * Primary method to process our test queries qa--qf
+ */
 static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
 {
   query_op op;
-
+  // extract the query op to get the query request params
   try {
     bufferlist::iterator it = in->begin();
     ::decode(op, it);
@@ -284,10 +179,11 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
 
   bufferlist bl;
 
+  // track the bufferlist read time (read_ns) vs. the processing time (eval_ns)
   uint64_t read_ns = 0;
   if (op.query != "d" || !op.use_index) {
     uint64_t start = getns();
-    int ret = cls_cxx_read(hctx, 0, 0, &bl);
+    int ret = cls_cxx_read(hctx, 0, 0, &bl);  // read entire object of n rows.
     if (ret < 0) {
       CLS_ERR("ERROR: reading obj %d", ret);
       return ret;
@@ -297,11 +193,12 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
 
   uint64_t eval_ns_start = getns();
 
+  // our test data is fixed size per col and uses tpch lineitem schema.
   const size_t row_size = 141;
   const char *rows = bl.c_str();
   const size_t num_rows = bl.length() / row_size;
 
-  bufferlist result_bl;
+  bufferlist result_bl;  // result set to be returned to client.
   result_bl.reserve(bl.length());
 
   const size_t order_key_field_offset = 0;
@@ -313,21 +210,21 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
   const size_t comment_field_offset = 97;
   const size_t comment_field_length = 44;
 
-  if (op.query == "a") {
+  if (op.query == "a") {  // count query on extended_price col
     size_t result_count = 0;
     for (size_t rid = 0; rid < num_rows; rid++) {
-      const char *row = rows + rid * row_size;
-      const char *vptr = row + extended_price_field_offset;
-      const double val = *(const double*)vptr;
-      if (val > op.extended_price) {
-        result_count++;
+      const char *row = rows + rid * row_size;  // offset to row
+      const char *vptr = row + extended_price_field_offset;  // offset to col
+      const double val = *(const double*)vptr;  // extract data as col type
+      if (val > op.extended_price) {  // apply filter
+        result_count++;  // counter of matching rows for this count(*) query 
         // when a predicate passes, add some extra work
         add_extra_row_cost(op.extra_row_cost);
       }
     }
-    ::encode(result_count, result_bl);
-  } else if (op.query == "b") {
-    if (op.projection) {
+    ::encode(result_count, result_bl);  // store count into our result set.
+  } else if (op.query == "b") {  // range query on extended_price col
+    if (op.projection) {  // only add (orderkey,linenum) col data to result set
       for (size_t rid = 0; rid < num_rows; rid++) {
         const char *row = rows + rid * row_size;
         const char *vptr = row + extended_price_field_offset;
@@ -349,7 +246,7 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
         }
       }
     }
-  } else if (op.query == "c") {
+  } else if (op.query == "c") {  // equality query on extended_price col
     if (op.projection) {
       for (size_t rid = 0; rid < num_rows; rid++) {
         const char *row = rows + rid * row_size;
@@ -372,12 +269,14 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
         }
       }
     }
-  } else if (op.query == "d") {
-    if (op.use_index) {
+  } else if (op.query == "d") {// point query on primary key (orderkey,linenum)
+    if (op.use_index) {  // if we have previously built an index on the PK.
+      // create the requested composite key from the op struct 
       uint64_t key = ((uint64_t)op.order_key) << 32;
-      key |= (uint32_t)op.line_number;
+      key |= (uint32_t)op.line_number; 
       const std::string strkey = u64tostr(key);
 
+      // key lookup in omap to get the row offset
       bufferlist row_offset_bl;
       int ret = cls_cxx_map_get_val(hctx, strkey, &row_offset_bl);
       if (ret < 0 && ret != -ENOENT) {
@@ -385,7 +284,7 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
         return ret;
       }
 
-      if (ret >= 0) {
+      if (ret >= 0) {  // found key
         size_t row_offset;
         bufferlist::iterator it = row_offset_bl.begin();
         try {
@@ -401,12 +300,13 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
           CLS_ERR("ERR stat %d", ret);
           return ret;
         }
-
+        // sanity check we won't try to read beyond obj size
         if ((row_offset + row_size) > size) {
           return -EIO;
         }
 
-        bufferlist bl;
+        // read just the row
+        bufferlist bl;  
         ret = cls_cxx_read(hctx, row_offset, row_size, &bl);
         if (ret < 0) {
           CLS_ERR("ERROR: reading obj %d", ret);
@@ -419,7 +319,8 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
         }
 
         const char *row = bl.c_str();
-
+        
+        // add to result set.
         if (op.projection) {
           result_bl.append(row + order_key_field_offset, 4);
           result_bl.append(row + line_number_field_offset, 4);
@@ -428,7 +329,7 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
         }
       }
 
-    } else {
+    } else {  // no index, look for matching row(s) and extract key cols.
       if (op.projection) {
         for (size_t rid = 0; rid < num_rows; rid++) {
           const char *row = rows + rid * row_size;
@@ -444,12 +345,12 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
             }
           }
         }
-      } else {
+      } else { // no index, look for matching row(s) and extract all cols.
         for (size_t rid = 0; rid < num_rows; rid++) {
           const char *row = rows + rid * row_size;
           const char *vptr = row + order_key_field_offset;
           const int order_key_val = *(const int*)vptr;
-          if (order_key_val == op.order_key) {
+          if (order_key_val == op.order_key) { 
             const char *vptr = row + line_number_field_offset;
             const int line_number_val = *(const int*)vptr;
             if (line_number_val == op.line_number) {
@@ -460,8 +361,8 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
         }
       }
     }
-  } else if (op.query == "e") {
-    if (op.projection) {
+  } else if (op.query == "e") {  // range query over multiple cols
+    if (op.projection) {  // look for matching row(s) and extract key cols.
       for (size_t rid = 0; rid < num_rows; rid++) {
         const char *row = rows + rid * row_size;
 
@@ -478,7 +379,7 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
           }
         }
       }
-    } else {
+    } else { // look for matching row(s) and extract all cols.
       for (size_t rid = 0; rid < num_rows; rid++) {
         const char *row = rows + rid * row_size;
 
@@ -495,8 +396,8 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
         }
       }
     }
-  } else if (op.query == "f") {
-    if (op.projection) {
+  } else if (op.query == "f") {  // regex query on comment cols
+    if (op.projection) {  // look for matching row(s) and extract key cols.
       RE2 re(op.comment_regex);
       for (size_t rid = 0; rid < num_rows; rid++) {
         const char *row = rows + rid * row_size;
@@ -509,7 +410,7 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
           add_extra_row_cost(op.extra_row_cost);
         }
       }
-    } else {
+    } else { // look for matching row(s) and extract all cols.
       RE2 re(op.comment_regex);
       for (size_t rid = 0; rid < num_rows; rid++) {
         const char *row = rows + rid * row_size;
@@ -525,170 +426,12 @@ static int query_op_op(cls_method_context_t hctx, bufferlist *in, bufferlist *ou
   } else {
     return -EINVAL;
   }
-
+  
   uint64_t eval_ns = getns() - eval_ns_start;
-
+  // store timings and result set into output BL
   ::encode(read_ns, *out);
   ::encode(eval_ns, *out);
   ::encode(result_bl, *out);
-
-  return 0;
-}
-
-static int regex_scan(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
-{
-  regex_scan_op op;
-
-  try {
-    bufferlist::iterator it = in->begin();
-    ::decode(op, it);
-  } catch (const buffer::error &err) {
-    CLS_ERR("ERROR: decoding regex scan op");
-    return -EINVAL;
-  }
-
-  bufferlist bl;
-  int ret = cls_cxx_read(hctx, 0, 0, &bl);
-  if (ret < 0) {
-    CLS_ERR("ERROR: reading obj %d", ret);
-    return ret;
-  }
-
-  const char *rows = bl.c_str();
-  size_t num_rows = bl.length() / op.row_size;
-
-  bufferlist rows_bl;
-  rows_bl.reserve(bl.length());
-  for (size_t r = 0; r < num_rows; r++) {
-    const char *row = rows + r * op.row_size;
-    const char *cptr = row + op.field_offset;
-    const std::string comment(cptr, op.field_length);
-    if (op.regex == "" || RE2::PartialMatch(comment, op.regex))
-      rows_bl.append(row, op.row_size);
-  }
-
-  ::encode(rows_bl, *out);
-
-  return 0;
-}
-
-static int scan(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
-{
-  scan_op op;
-
-  try {
-    bufferlist::iterator it = in->begin();
-    ::decode(op, it);
-  } catch (const buffer::error &err) {
-    CLS_ERR("ERROR: decoding scan op");
-    return -EINVAL;
-  }
-
-  bool index_skipped = false;
-  uint64_t buffer[8192];
-  bufferlist rows_bl;
-
-  if (op.use_index) {
-#if 0
-    struct tab_index idx;
-
-    bufferlist hdrbl;
-    int ret = cls_cxx_map_read_header(hctx, &hdrbl);
-    if (ret < 0) {
-      CLS_ERR("ERROR: reading map hdr %d", ret);
-      return ret;
-    }
-
-    if (hdrbl.length() == 0) {
-      CLS_ERR("ERROR: no header");
-      return -EINVAL;
-    }
-
-    try {
-      bufferlist::iterator it = hdrbl.begin();
-      ::decode(idx, it);
-    } catch (const buffer::error &err) {
-      CLS_ERR("ERROR: decoding index");
-      return -EIO;
-    }
-#endif
-
-    bufferlist attr_bl;
-    int ret = cls_cxx_getxattr(hctx, "min", &attr_bl);
-    if (ret < 0) {
-      CLS_ERR("ERROR: reading min attr: %d", ret);
-      return ret;
-    }
-
-    if (attr_bl.length() == 0) {
-      CLS_ERR("ERROR: no min attr");
-      return -EINVAL;
-    }
-
-    uint64_t min_val;
-    try {
-      bufferlist::iterator it = attr_bl.begin();
-      ::decode(min_val, it);
-    } catch (const buffer::error&) {
-      CLS_ERR("ERROR: decoding min attr");
-      return -EIO;
-    }
-
-    if (op.max_val < min_val) {
-      index_skipped = true;
-      ::encode(index_skipped, *out);
-      ::encode(rows_bl, *out);
-      return 0;
-    }
-  }
-
-  bufferlist bl;
-  int ret = cls_cxx_read(hctx, 0, 0, &bl);
-  if (ret < 0) {
-    CLS_ERR("ERROR: reading obj %d", ret);
-    return ret;
-  }
-
-  const uint64_t *rows = (uint64_t*)bl.c_str();
-  const uint64_t row_count = bl.length() / sizeof(uint64_t);
-
-  /*
-   * TODO:
-   *  - can we use loop unrolling, vector ops, or other techniques to speed
-   *  this up?
-   */
-  if (op.naive) {
-    if (op.preallocate) {
-      rows_bl.reserve(bl.length());
-    }
-    for (unsigned i = 0; i < row_count; i++) {
-      const uint64_t row_val = rows[i];
-      if (row_val < op.max_val) {
-        rows_bl.append((char*)&row_val, sizeof(row_val));
-      }
-    }
-  } else {
-    rows_bl.reserve(bl.length());
-    size_t curr_entry = 0;
-    for (unsigned i = 0; i < row_count; i++) {
-      const uint64_t row_val = rows[i];
-      if (row_val < op.max_val) {
-        buffer[curr_entry] = row_val;
-        if (++curr_entry == 8192) {
-          rows_bl.append((char*)buffer,
-              sizeof(uint64_t) * curr_entry);
-          curr_entry = 0;
-        }
-      }
-    }
-    if (curr_entry > 0) {
-      rows_bl.append((char*)buffer,
-          sizeof(uint64_t) * curr_entry);
-    }
-  }
-
-  ::encode(index_skipped, *out);
-  ::encode(rows_bl, *out);
 
   return 0;
 }
@@ -699,23 +442,8 @@ void __cls_init()
 
   cls_register("tabular", &h_class);
 
-  cls_register_cxx_method(h_class, "read",
-      CLS_METHOD_RD | CLS_METHOD_WR, read, &h_read);
-
-  cls_register_cxx_method(h_class, "scan",
-      CLS_METHOD_RD | CLS_METHOD_WR, scan, &h_scan);
-
-  cls_register_cxx_method(h_class, "add",
-      CLS_METHOD_RD | CLS_METHOD_WR, add, &h_add);
-
-  cls_register_cxx_method(h_class, "regex_scan",
-      CLS_METHOD_RD, regex_scan, &h_regex_scan);
-
   cls_register_cxx_method(h_class, "query_op",
       CLS_METHOD_RD, query_op_op, &h_query_op);
-
-  cls_register_cxx_method(h_class, "test_par",
-      CLS_METHOD_RD, test_par, &h_test_par);
 
   cls_register_cxx_method(h_class, "build_index",
       CLS_METHOD_RD | CLS_METHOD_WR, build_index, &h_build_index);
--- src/cls/tabular/cls_tabular.h
+++ src/cls/tabular/cls_tabular.h
@@ -13,67 +13,15 @@
 #define CLS_TABULAR_H
 #include "include/types.h"
 
-struct scan_op {
-  uint64_t max_val;
-  bool use_index;
-  bool naive;
-  bool preallocate;
-
-  scan_op() {}
-
-  void encode(bufferlist& bl) const {
-    ENCODE_START(1, 1, bl);
-    ::encode(max_val, bl);
-    ::encode(use_index, bl);
-    ::encode(naive, bl);
-    ::encode(preallocate, bl);
-    ENCODE_FINISH(bl);
-  }
-
-  void decode(bufferlist::iterator& bl) {
-    DECODE_START(1, bl);
-    ::decode(max_val, bl);
-    ::decode(use_index, bl);
-    ::decode(naive, bl);
-    ::decode(preallocate, bl);
-    DECODE_FINISH(bl);
-  }
-};
-WRITE_CLASS_ENCODER(scan_op)
-
-struct regex_scan_op {
-  size_t row_size;
-  size_t field_offset;
-  size_t field_length;
-  std::string regex;
-
-  regex_scan_op() {}
-
-  void encode(bufferlist& bl) const {
-    ENCODE_START(1, 1, bl);
-    ::encode(row_size, bl);
-    ::encode(field_offset, bl);
-    ::encode(field_length, bl);
-    ::encode(regex, bl);
-    ENCODE_FINISH(bl);
-  }
-
-  void decode(bufferlist::iterator& bl) {
-    DECODE_START(1, bl);
-    ::decode(row_size, bl);
-    ::decode(field_offset, bl);
-    ::decode(field_length, bl);
-    ::decode(regex, bl);
-    DECODE_FINISH(bl);
-  }
-};
-WRITE_CLASS_ENCODER(regex_scan_op)
-
+/*
+ * Stores the query request parameters.  This is encoded by the client and 
+ * decoded by server (osd node) for query processing. 
+ */
 struct query_op {
   // query name
   std::string query;
 
-  // query parameters
+  // query parameters (uses tpch lineitem schema)
   double extended_price;
   int order_key;
   int line_number;
