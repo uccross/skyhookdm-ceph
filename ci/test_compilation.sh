d69fcdacfc2f894a0f2e921bc2e6bc54da57e28b
Michael Sevilla <mikesevilla3@gmail.com>
2019-01-02 10:12:25 -0800

    ci/test: fix directory name in ci (#17)
    Signed-off-by: Michael A. Sevilla <mikesevilla@gmail.com>

--- ci/test_compilation.sh
+++ ci/test_compilation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -ex
 
-docker run -di --name test -v /home/travis/build/uccross/skyhookdb-ceph:/ceph cephbuilder/ceph:${RELEASE}
+docker run -di --name test -v /home/travis/build/uccross/skyhook-ceph:/ceph cephbuilder/ceph:${RELEASE}
 docker exec test /bin/bash -c "mkdir build && cd build && cmake -DWITH_RADOSGW=OFF .."
 docker exec test /bin/bash -c "cd build && make -j2 ceph-osd osdc librados cls_tabular run-query ceph_test_skyhook_query"
 # to run vstart, we additionally need: 
