c5c382e6539b58d9f47bfad06c3ad98f24a0997d
Kathryn Dahlgren <kmdahlgr@ucsc.edu>
2019-02-03 22:56:31 -0800

    implemented split on pattern.

--- src/cls/split/cls_split.cc
+++ src/cls/split/cls_split.cc
@@ -19,8 +19,8 @@ namespace Split {
     switch( split_op_id )
     {
       case 1  : split_12( blist_in, blist_out0, blist_out1 ) ;          break ;
-      //case 2  : split_pattern( blist_in, blist_out0, blist_out1, "" ) ; break ;
-      default : split_noop( blist_in, blist_out0, blist_out1 ) ;          break ;
+      case 2  : split_pattern( blist_in, blist_out0, blist_out1, "" ) ; break ;
+      default : split_noop( blist_in, blist_out0, blist_out1 ) ;        break ;
     }
 
     return 0 ;
@@ -77,6 +77,37 @@ namespace Split {
     std::cout << "  EXECUTING split_pattern()" << std::endl ;
     std::cout << "    pattern   : " << pattern << std::endl ;
     std::cout << std::endl ;
+
+    auto data = (std::string)blist_in->c_str() ;
+    auto pos  = data.find( "s" ) ;
+    std::cout << data << std::endl ;
+    std::cout << pos << std::endl ;
+
+    // this is easier with a bufferptr
+    ceph::bufferptr bptr_in ;
+    librados::bufferlist::iterator i( &*blist_in ) ;
+    i.copy_deep( blist_in->length(), bptr_in ) ;
+
+    // fill first list
+    for( unsigned int i = 0; i < pos; i++ )
+    {
+      blist_out0->append( bptr_in[ i ] ) ;
+    }
+
+    // fill second list
+    for( unsigned int i = pos; i < blist_in->length(); i++ )
+    {
+      blist_out1->append( bptr_in[ i ] ) ;
+    }
+
+    std::cout << "METHOD check:"      << std::endl ;
+    std::cout << blist_in->c_str()    << std::endl ;
+    std::cout << blist_in->length()   << std::endl ;
+    std::cout << blist_out0->c_str()  << std::endl ;
+    std::cout << blist_out0->length() << std::endl ;
+    std::cout << blist_out1->c_str()  << std::endl ;
+    std::cout << blist_out1->length() << std::endl ;
+
     return 0 ;
   }
 
