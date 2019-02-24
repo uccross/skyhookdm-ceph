298c123be8184fb87916de0b8e6720915a559429
Kathryn Dahlgren <kmdahlgr@ucsc.edu>
2019-02-23 19:53:50 -0800

    splits with vector of pairs.

--- src/cls/split/cls_split.cc
+++ src/cls/split/cls_split.cc
@@ -1,126 +1,131 @@
 #include <iostream>
 #include "cls_split.h"
 
-namespace Split {
+// ==================== //
+//      CEPH SPLIT      //
+// ==================== //
+int ceph_split( librados::bufferlist* blist_in,
+           librados::bufferlist* blist_out0, 
+           librados::bufferlist* blist_out1, 
+           int split_op_id,
+           const char* split_pattern )
+{
+  std::cout << "===================================================" << std::endl ;
+  std::cout << "  EXECUTING split()" << std::endl ;
+  std::cout << "    split_op_id : " << split_op_id       << std::endl ;
+  std::cout << std::endl ;
 
-  // =============== //
-  //      SPLIT      //
-  // =============== //
-  int split( librados::bufferlist* blist_in,
-             librados::bufferlist* blist_out0, 
-             librados::bufferlist* blist_out1, 
-             int split_op_id )
+  switch( split_op_id )
   {
-    std::cout << "===================================================" << std::endl ;
-    std::cout << "  EXECUTING split()" << std::endl ;
-    std::cout << "    split_op_id : " << split_op_id       << std::endl ;
-    std::cout << std::endl ;
-
-    switch( split_op_id )
-    {
-      case 1  : split_12( blist_in, blist_out0, blist_out1 ) ;          break ;
-      case 2  : split_pattern( blist_in, blist_out0, blist_out1, "" ) ; break ;
-      default : split_noop( blist_in, blist_out0, blist_out1 ) ;        break ;
-    }
-
-    return 0 ;
+    case 1  : ceph_split_12( blist_in, blist_out0, blist_out1 ) ;                     break ;
+    case 2  : ceph_split_pattern( blist_in, blist_out0, blist_out1, split_pattern ) ; break ;
+    default : ceph_split_noop( blist_in, blist_out0, blist_out1 ) ;                   break ;
   }
 
-  // ================== //
-  //      SPLIT 12      //
-  // ================== //
-  int split_12( librados::bufferlist* blist_in,
-                librados::bufferlist* blist_out0, 
+  return 0 ;
+}
+
+// ======================= //
+//      CEPH SPLIT 12      //
+// ======================= //
+int ceph_split_12( librados::bufferlist* blist_in,
+                   librados::bufferlist* blist_out0, 
+                   librados::bufferlist* blist_out1 )
+{
+  std::cout << "............................" << std::endl ;
+  std::cout << "  EXECUTING ceph_split_12()" << std::endl ;
+  std::cout << std::endl ;
+
+  std::vector< dataset > decoded_table ;
+  librados::bufferlist::iterator biter( &*blist_in ) ;
+  ::decode( decoded_table, biter ) ;
+
+  int midpoint = decoded_table[0].data.size() / 2 ;
+
+  dataset left ;
+  dataset right ;
+
+  for( int i = 0; i < midpoint; i++ ) {
+    left.data.push_back( decoded_table[0].data[i] ) ;
+  }
+  for( unsigned int i = midpoint; i < decoded_table[0].data.size(); i++ ) {
+    right.data.push_back( decoded_table[0].data[i] ) ;
+  }
+
+  //std::cout << left.toString() << std::endl ;
+  //std::cout << right.toString() << std::endl ;
+
+  std::vector< dataset > left_table ;
+  left_table.push_back( left ) ;
+  ::encode( left_table, *blist_out0 ) ;
+
+  std::vector< dataset > right_table ;
+  right_table.push_back( right ) ;
+  ::encode( right_table, *blist_out1 ) ;
+
+  return 0 ;
+}
+
+// ============================ //
+//      CEPH SPLIT PATTERN      //
+// ============================ //
+int ceph_split_pattern( librados::bufferlist* blist_in, 
+                        librados::bufferlist* blist_out0, 
+                        librados::bufferlist* blist_out1, 
+                        const char* split_pattern )
+{
+  std::cout << "............................" << std::endl ;
+  std::cout << "  EXECUTING split_pattern()" << std::endl ;
+  std::cout << "    split_pattern   : " << split_pattern << std::endl ;
+  std::cout << std::endl ;
+
+  std::vector< dataset > decoded_table ;
+  librados::bufferlist::iterator biter( &*blist_in ) ;
+  ::decode( decoded_table, biter ) ;
+
+//  int pos = -1 ;
+//  for( unsigned int i = 0; i < decoded_table[0].data.size(); i++ ) {
+//    auto apair = decoded_table[0].data[i] ;
+//    if( apair.first == split_pattern ) {
+//      pos = i ;
+//      break ;
+//    }
+//  } 
+  int pos = 2 ;
+
+  dataset left ;
+  dataset right ;
+
+  for( int i = 0; i < pos; i++ ) {
+    left.data.push_back( decoded_table[0].data[i] ) ;
+  }
+  for( unsigned int i = pos; i < decoded_table[0].data.size(); i++ ) {
+    right.data.push_back( decoded_table[0].data[i] ) ;
+  }
+
+  std::cout << left.toString() << std::endl ;
+  std::cout << right.toString() << std::endl ;
+
+  std::vector< dataset > left_table ;
+  left_table.push_back( left ) ;
+  ::encode( left_table, *blist_out0 ) ;
+
+  std::vector< dataset > right_table ;
+  right_table.push_back( right ) ;
+  ::encode( right_table, *blist_out1 ) ;
+
+  return 0 ;
+}
+
+// ========================= //
+//      CEPH SPLIT NOOP      //
+// ========================= //
+int ceph_split_noop( librados::bufferlist* blist_in, 
+                librados::bufferlist* blist_out0,
                 librados::bufferlist* blist_out1 )
-  {
-    std::cout << "............................" << std::endl ;
-    std::cout << "  EXECUTING split_12()" << std::endl ;
-    std::cout << std::endl ;
-
-    // this is easier with a bufferptr
-    ceph::bufferptr bptr_in ;
-    librados::bufferlist::iterator i( &*blist_in ) ;
-    i.copy_deep( blist_in->length(), bptr_in ) ;
-
-    // fill first list
-    for( unsigned int i = 0; i < blist_in->length() / 2; i++ )
-    {
-      blist_out0->append( bptr_in[ i ] ) ;
-    }
-
-    // fill second list
-    for( unsigned int i = blist_in->length() / 2; i < blist_in->length(); i++ )
-    {
-      blist_out1->append( bptr_in[ i ] ) ;
-    }
-
-    return 0 ;
-  }
-
-  // ======================= //
-  //      SPLIT PATTERN      //
-  // ======================= //
-  int split_pattern( librados::bufferlist* blist_in, 
-                     librados::bufferlist* blist_out0, 
-                     librados::bufferlist* blist_out1, 
-                     const char* pattern )
-  {
-    std::cout << "............................" << std::endl ;
-    std::cout << "  EXECUTING split_pattern()" << std::endl ;
-    std::cout << "    pattern   : " << pattern << std::endl ;
-    std::cout << std::endl ;
-
-    auto data = (std::string)blist_in->c_str() ;
-    auto pos  = data.find( "s" ) ;
-
-    // this is easier with a bufferptr
-    ceph::bufferptr bptr_in ;
-    librados::bufferlist::iterator i( &*blist_in ) ;
-    i.copy_deep( blist_in->length(), bptr_in ) ;
-
-    // fill first list
-    for( unsigned int i = 0; i < pos; i++ )
-    {
-      blist_out0->append( bptr_in[ i ] ) ;
-    }
-
-    // fill second list
-    for( unsigned int i = pos; i < blist_in->length(); i++ )
-    {
-      blist_out1->append( bptr_in[ i ] ) ;
-    }
-
-    return 0 ;
-  }
-
-  // ==================== //
-  //      SPLIT NOOP      //
-  // ==================== //
-  int split_noop( librados::bufferlist* blist_in, 
-                  librados::bufferlist* blist_out0,
-                  librados::bufferlist* blist_out1 )
-  {
-    std::cout << "............................" << std::endl ;
-    std::cout << "  EXECUTING split_noop()" << std::endl ;
-    std::cout << std::endl ;
-
-    // this is easier with a bufferptr
-    ceph::bufferptr bptr_in ;
-    librados::bufferlist::iterator i( &*blist_in ) ;
-    i.copy_deep( blist_in->length(), bptr_in ) ;
-
-    // fill first list
-    for( unsigned int i = 0; i < blist_in->length(); i++ )
-    {
-      blist_out0->append( bptr_in[ i ] ) ;
-    }
-
-    // fill second list
-    for( unsigned int i = 0; i < blist_in->length(); i++ )
-    {
-      blist_out1->append( bptr_in[ i ] ) ;
-    }
-
-    return 0 ;
-  }
+{
+  std::cout << "............................" << std::endl ;
+  std::cout << "  EXECUTING split_noop()" << std::endl ;
+  std::cout << std::endl ;
+  return 0 ;
 }
--- src/progly/run-split.cc
+++ src/progly/run-split.cc
@@ -35,43 +35,109 @@ int main(int argc, char **argv)
   librados::bufferlist id1_blist_in ;
   librados::bufferlist id1_blist_out0 ;
   librados::bufferlist id1_blist_out1 ;
-  id1_blist_in.append( "teststr", 7 ) ;
 
-  Split::split( &id1_blist_in, &id1_blist_out0, &id1_blist_out1, 1 ) ;
-  assert( id1_blist_in.length()   == 7 ) ;
-  assert( id1_blist_out0.length() == 3 ) ;
-  assert( id1_blist_out1.length() == 4 ) ;
-  assert( ((std::string)id1_blist_in.c_str()).compare(   "teststr" ) == 0 ) ;
-  assert( ((std::string)id1_blist_out0.c_str()).compare( "tes"     ) == 0 ) ;
-  assert( ((std::string)id1_blist_out1.c_str()).compare( "tstr"    ) == 0 ) ;
+  std::string str_split_12 = "teststr" ;
+
+  std::vector<std::pair<std::string, int>> data_split_12 ;
+  dataset ds_split_12 ;
+
+  for( unsigned int i = 0; i < str_split_12.length(); i++ ) {
+    std::string astr ;
+    auto achar = str_split_12.c_str()[ i ] ;
+    astr.push_back( achar ) ;
+    ds_split_12.data.push_back( std::make_pair( astr, 1 ) ) ;
+  }
+
+  std::vector< dataset > table_split_12 ;
+
+  table_split_12.push_back( ds_split_12 ) ;
+
+  ::encode( table_split_12, id1_blist_in ) ;
+  ceph_split( &id1_blist_in, &id1_blist_out0, &id1_blist_out1, 1 ) ;
+
+  std::vector< dataset > decoded_id1_blist_in ;
+  librados::bufferlist::iterator decoded_id1_biter_in( &id1_blist_in ) ;
+  ::decode( decoded_id1_blist_in, decoded_id1_biter_in ) ;
+  std::vector< dataset > decoded_id1_blist_out0 ;
+  librados::bufferlist::iterator decoded_id1_biter_out0( &id1_blist_out0 ) ;
+  ::decode( decoded_id1_blist_out0, decoded_id1_biter_out0 ) ;
+  std::vector< dataset > decoded_id1_blist_out1 ;
+  librados::bufferlist::iterator decoded_id1_biter_out1( &id1_blist_out1 ) ;
+  ::decode( decoded_id1_blist_out1, decoded_id1_biter_out1 ) ;
+
+  std::cout << decoded_id1_blist_in[0].toString() << std::endl ;
+  std::cout << decoded_id1_blist_out0[0].toString() << std::endl ;
+  std::cout << decoded_id1_blist_out1[0].toString() << std::endl ;
+
+  assert( decoded_id1_blist_in[0].toString()   == "dataset :  t, 1  e, 1  s, 1  t, 1  s, 1  t, 1  r, 1" ) ;
+  assert( decoded_id1_blist_out0[0].toString() == "dataset :  t, 1  e, 1  s, 1" ) ;
+  assert( decoded_id1_blist_out1[0].toString() == "dataset :  t, 1  s, 1  t, 1  r, 1" ) ;
 
   // split_pattern
   librados::bufferlist id2_blist_in ;
   librados::bufferlist id2_blist_out0 ;
   librados::bufferlist id2_blist_out1 ;
-  id2_blist_in.append( "teststr", 7 ) ;
 
-  Split::split( &id1_blist_in, &id2_blist_out0, &id2_blist_out1, 2 ) ;
-  assert( id2_blist_in.length()   == 7 ) ;
-  assert( id2_blist_out0.length() == 2 ) ;
-  assert( id2_blist_out1.length() == 5 ) ;
-  assert( ((std::string)id2_blist_in.c_str()).compare(   "teststr" ) == 0 ) ;
-  assert( ((std::string)id2_blist_out0.c_str()).compare( "te"      ) == 0 ) ;
-  assert( ((std::string)id2_blist_out1.c_str()).compare( "ststr"   ) == 0 ) ;
+  std::string str_split_pattern = "teststr" ;
+
+  std::vector<std::pair<std::string, int>> data_split_pattern ;
+  dataset ds_split_pattern ;
+
+  for( unsigned int i = 0; i < str_split_pattern.length(); i++ ) {
+    std::string astr ;
+    auto achar = str_split_pattern.c_str()[ i ] ;
+    astr.push_back( achar ) ;
+    ds_split_pattern.data.push_back( std::make_pair( astr, 1 ) ) ;
+  }
+
+  std::vector< dataset > table_split_pattern ;
+
+  table_split_pattern.push_back( ds_split_pattern ) ;
+
+  ::encode( table_split_pattern, id2_blist_in ) ;
+  ceph_split( &id2_blist_in, &id2_blist_out0, &id2_blist_out1, 2, "s" ) ;
+
+  std::vector< dataset > decoded_id2_blist_in ;
+  librados::bufferlist::iterator decoded_id2_biter_in( &id2_blist_in ) ;
+  ::decode( decoded_id2_blist_in, decoded_id2_biter_in ) ;
+  std::vector< dataset > decoded_id2_blist_out0 ;
+  librados::bufferlist::iterator decoded_id2_biter_out0( &id2_blist_out0 ) ;
+  ::decode( decoded_id2_blist_out0, decoded_id2_biter_out0 ) ;
+  std::vector< dataset > decoded_id2_blist_out1 ;
+  librados::bufferlist::iterator decoded_id2_biter_out1( &id2_blist_out1 ) ;
+  ::decode( decoded_id2_blist_out1, decoded_id2_biter_out1 ) ;
+
+  //std::cout << decoded_id2_blist_in[0].toString() << std::endl ;
+  //std::cout << decoded_id2_blist_out0[0].toString() << std::endl ;
+  //std::cout << decoded_id2_blist_out1[0].toString() << std::endl ;
+
+  assert( decoded_id2_blist_in[0].toString()   == "dataset :  t, 1  e, 1  s, 1  t, 1  s, 1  t, 1  r, 1" ) ;
+  assert( decoded_id2_blist_out0[0].toString() == "dataset :  t, 1  e, 1" ) ;
+  assert( decoded_id2_blist_out1[0].toString() == "dataset :  s, 1  t, 1  s, 1  t, 1  r, 1" ) ;
 
   // split_noop
   librados::bufferlist id0_blist_in ;
   librados::bufferlist id0_blist_out0 ;
   librados::bufferlist id0_blist_out1 ;
-  id0_blist_in.append( "teststr", 7 ) ;
 
-  Split::split( &id0_blist_in, &id0_blist_out0, &id0_blist_out1, 0 ) ;
-  assert( id0_blist_in.length()   == 7 ) ;
-  assert( id0_blist_out0.length() == 7 ) ;
-  assert( id0_blist_out1.length() == 7 ) ;
-  assert( ((std::string)id0_blist_in.c_str()).compare(   "teststr" ) == 0 ) ;
-  assert( ((std::string)id0_blist_out0.c_str()).compare( "teststr" ) == 0 ) ;
-  assert( ((std::string)id0_blist_out1.c_str()).compare( "teststr" ) == 0 ) ;
+  std::string str_split_noop = "teststr" ;
+
+  std::vector<std::pair<std::string, int>> data_split_noop ;
+  dataset ds_split_noop ;
+
+  for( unsigned int i = 0; i < str_split_noop.length(); i++ ) {
+    std::string astr ;
+    auto achar = str_split_noop.c_str()[ i ] ;
+    astr.push_back( achar ) ;
+    ds_split_noop.data.push_back( std::make_pair( astr, 1 ) ) ;
+  }
+
+  std::vector< dataset > table_split_noop ;
+
+  table_split_noop.push_back( ds_split_noop ) ;
+
+  ::encode( table_split_noop, id0_blist_in ) ;
+  ceph_split( &id0_blist_in, &id0_blist_out0, &id0_blist_out1, 0 ) ;
 
   std::cout << "...splits done. phew!" << std::endl ;
   return 0 ;
