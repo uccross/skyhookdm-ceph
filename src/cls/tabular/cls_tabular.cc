5177a54c45e7670bdd2a9990a95b7171b584d87b
nitesh1994 <niteshks30@gmail.com>
2019-03-19 18:59:43 -0700

    changes to store fb_seq_num in xattr

--- src/cls/tabular/cls_tabular.cc
+++ src/cls/tabular/cls_tabular.cc
@@ -78,7 +78,23 @@ int build_sky_index(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
     // a ceph property encoding the len of each bl in front of the bl,
     // seems to be an int32 currently.
     const int ceph_bl_encoding_len = sizeof(int32_t);
-    int fb_seq_num = 0;  // TODO: get this from a stable counter.
+
+    bufferlist fb_bl2;
+    int fb_seq_num;
+    int r1 = cls_cxx_getxattr(hctx, "fb_seq_num", &fb_bl2);
+    if (r1 == -ENOENT || r1 == -ENODATA) {
+        fb_seq_num = 0;  // TODO: get this from a stable counter.
+        // If fb_seq_num is not present then insert it in xattr. 
+    }
+    else if( r1 < 0 ) {
+        return r1;
+    }
+    else {
+        bufferlist::iterator it = fb_bl2.begin();
+        ::decode(fb_seq_num,it);
+        CLS_LOG(20,"Read from xattr %d", fb_seq_num);
+    }
+    
 
     std::string key_prefix;
     std::string key_data;
@@ -345,6 +361,13 @@ int build_sky_index(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
             return ret;
         }
     }
+
+    // Update counter and Insert fb_seq_num to xattr
+    fb_seq_num +=1;
+    ::encode(fb_seq_num, fb_bl2);
+    int r = cls_cxx_setxattr(hctx, "fb_seq_num", &fb_bl2);
+    if( r < 0 )
+        return r;
     return 0;
 }
 
