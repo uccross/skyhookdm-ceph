8116c2d602b49f7f657e9644675dfbd9e54a5010
Kathryn Dahlgren <kmdahlgr@ucsc.edu>
2019-03-21 10:29:14 -0700

    populating custom fb with data and writing and reading fb bufferlist to an object.

--- src/cls/tabular/cls_transform_utils.cc
+++ src/cls/tabular/cls_transform_utils.cc
@@ -26,10 +26,43 @@ void test() {
   ::encode( bl1, bl_seq ) ;
   ::encode( bl2, bl_seq ) ;
 
-  ceph::bufferlist::iterator it = bl_seq.begin() ;
-  while( it.get_remaining() > 0 ) {
+  // save to ceph object
+  // connect to rados
+  librados::Rados cluster;
+  cluster.init(NULL);
+  cluster.conf_read_file(NULL);
+  int ret = cluster.connect();
+  checkret(ret, 0);
+
+  // open pool
+  librados::IoCtx ioctx ;
+  ret = cluster.ioctx_create( "tpchflatbuf" , ioctx ) ;
+  checkret( ret, 0 ) ;
+
+  librados::bufferlist bufl ;
+  bufl.append( "asdf" ) ;
+
+  // write bl_seq
+  const char *obj_name  = "blah" ;
+  //ret = ioctx.write_full( obj_name, bufl ) ;
+  bufferlist::iterator p = bl_seq.begin();
+  size_t i = p.get_remaining() ;
+  std::cout << i << std::endl ;
+  ret = ioctx.write( obj_name, bl_seq, i, 0 ) ;
+  checkret(ret, 0);
+
+  // read bl_seq
+  librados::bufferlist wrapped_bl_seq ;
+  int num_bytes_read = ioctx.read( obj_name, wrapped_bl_seq, (size_t)0, (uint64_t)0 ) ;
+
+  //ceph::bufferlist::iterator it = bl_seq.begin() ;
+  //std::cout << it.get_remaining() << std::endl ;
+  ceph::bufferlist::iterator it_wrapped = wrapped_bl_seq.begin() ;
+  std::cout << it_wrapped.get_remaining() << std::endl ;
+
+  while( it_wrapped.get_remaining() > 0 ) {
     ceph::bufferlist bl ;
-    ::decode( bl, it ) ; // this decrements get_remaining by moving iterator
+    ::decode( bl, it_wrapped ) ; // this decrements get_remaining by moving iterator
 
     const char* fb = bl.c_str() ;
     auto root = Tables::GetRecord( fb ) ;
@@ -47,69 +80,7 @@ void test() {
     std::cout << "att3 : " << att3_read << std::endl ;
   }
 
-//  auto bl1 = record1() ;
-//  auto bl2 = record2() ;
-//
-//  ::encode( bl0, bl_seq ) ;
-//  ::encode( bl1, bl_seq ) ;
-//  ::encode( bl2, bl_seq ) ;
-//
-//  // save to file
-//  //bool result = flatbuffers::SaveFile( filename,
-//  //                                     (const char *) builder.GetBufferPointer(),
-//  //                                     (size_t) builder.GetSize(), true);
-//
-//  // save to ceph object
-//  // connect to rados
-//  librados::Rados cluster;
-//  cluster.init(NULL);
-//  cluster.conf_read_file(NULL);
-//  int ret = cluster.connect();
-//  checkret(ret, 0);
-//
-//  // open pool
-//  librados::IoCtx ioctx ;
-//  ret = cluster.ioctx_create( "tpchflatbuf" , ioctx ) ;
-//  checkret( ret, 0 ) ;
-//
-//  // write bl_seq
-//  const char *obj_name  = "blah" ;
-//  ret = ioctx.write_full( obj_name, bl_seq ) ;
-//  checkret(ret, 0);
-//
-//  //Create I/O Completion.
-//  librados::AioCompletion *read_completion = librados::Rados::aio_create_completion();
-//  librados::bufferlist wrapped_bl_seq ;
-//  ret = ioctx.aio_read( obj_name, read_completion, &wrapped_bl_seq, 0, 0 ) ;
-//  checkret( ret, 0 ) ;
-//
-//  uint64_t off = 0 ;
-//  ceph::bufferlist::iterator it = wrapped_bl_seq.begin() ;
-//  uint64_t obj_len = it.get_remaining() ;
-//  std::cout << obj_len << std::endl ;
-//  while( it.get_remaining() > 0 ) {
-//    off = obj_len - it.get_remaining() ;
-//    ceph::bufferlist bl ;
-//    ::decode( bl, it ) ;
-//
-//    const char* fb = bl.c_str() ;
-//    auto root = Tables::GetRecord( fb ) ;
-//
-//    auto table_name = root->table_name() ;
-//    auto att0_read = root->att0() ;
-//    auto att1_read = root->att1() ;
-//    auto att2_read = root->att2() ;
-//    auto att3_read = root->att3() ;
-//
-//    std::cout << "table_name : " << table_name << std::endl ;
-//    std::cout << "att0 : " << att0_read << std::endl ;
-//    std::cout << "att1 : " << att1_read << std::endl ;
-//    std::cout << "att2 : " << att2_read << std::endl ;
-//    std::cout << "att3 : " << att3_read << std::endl ;
-//
-//  }
-//
-//  ioctx.close();
+  ioctx.close();
 }
 
 librados::bufferlist record0() {
