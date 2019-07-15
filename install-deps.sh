26f847c035c9e4b9a092a503d54b5cc276d070ee
Ashay Shirwadkar <ashayshirwadkar12@gmail.com>
2019-07-15 11:18:59 +0530

    Update install-deps.sh
    Added  lsb-release dependency.

--- install-deps.sh
+++ install-deps.sh
@@ -117,14 +117,14 @@ else
     debian|ubuntu|devuan)
         echo "Using apt-get to install dependencies"
         # Depdencies for Apache Arrow
-        $SUDO apt-get install -y curl
+        $SUDO apt-get install -y curl lsb-release
         curl https://dist.apache.org/repos/dist/dev/arrow/KEYS | $SUDO apt-key add -
         $SUDO tee /etc/apt/sources.list.d/apache-arrow.list <<APT_LINE
 deb [arch=amd64] https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
 deb-src https://dl.bintray.com/apache/arrow/$(lsb_release --id --short | tr 'A-Z' 'a-z')/ $(lsb_release --codename --short) main
 APT_LINE
         $SUDO apt-get update -y
-        $SUDO apt-get install -y lsb-release devscripts equivs
+        $SUDO apt-get install -y devscripts equivs
         $SUDO apt-get install -y dpkg-dev gcc apt-transport-https gnupg libarrow-dev libparquet-dev
         case "$VERSION" in
             *Trusty*)
