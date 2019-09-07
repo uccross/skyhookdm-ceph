e677a1fa175f64e0c21e3ce9690df2aef6e0c8c7
jlefevre <jlefevre@soe.ucsc.edu>
2019-09-07 16:10:29 -0700

    fixed obj file name for arrow 10MB obj.

--- src/progly/cloudlab_cluster_setup/load-wrapper-10MB.sh
+++ src/progly/cloudlab_cluster_setup/load-wrapper-10MB.sh
@@ -12,10 +12,10 @@ pool_ncols100_10MB_arrow="ncols100.10MB.arrow"
 pool_lineitem_10MB_fbx="lineitem.10MB.fbx"
 pool_lineitem_10MB_arrow="lineitem.10MB.arrow"
 echo `date`
-bash load-data.sh $pool_ncols100_10MB_fbx     fbx.ncols100.10MB.25Krows.obj.0  $numosds $numobjs;
-bash load-data.sh  $pool_ncols100_10MB_arrow arrow.ncols100.10MB   $numosds $numobjs;
-bash load-data.sh  $pool_lineitem_10MB_fbx     fbx.lineitem.10MB.75Krows.obj.0   $numosds $numobjs;
-bash load-data.sh  $pool_lineitem_10MB_arrow  arrow.lineitem.10MB.75Krows.obj.0  $numosds $numobjs;
+bash load-data.sh  $pool_ncols100_10MB_fbx   fbx.ncols100.10MB.25Krows.obj.0   $numosds $numobjs;
+bash load-data.sh  $pool_ncols100_10MB_arrow arrow.ncols100.10MB.25Krows.obj.0 $numosds $numobjs;
+bash load-data.sh  $pool_lineitem_10MB_fbx   fbx.lineitem.10MB.75Krows.obj.0   $numosds $numobjs;
+bash load-data.sh  $pool_lineitem_10MB_arrow arrow.lineitem.10MB.75Krows.obj.0 $numosds $numobjs;
 echo "loaded 10MB data into pools:"
 echo  $pool_ncols100_10MB_fbx
 echo  $pool_ncols100_10MB_arrow
