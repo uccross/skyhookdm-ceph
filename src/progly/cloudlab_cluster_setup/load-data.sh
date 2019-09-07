ef95b97f4bdb69c9cdd22ac44c28f34c9306fd66
jlefevre <jlefevre@soe.ucsc.edu>
2019-09-07 15:24:00 -0700

    Updated load wrappers for all 10MB obj data formats.

--- src/progly/cloudlab_cluster_setup/load-data.sh
+++ src/progly/cloudlab_cluster_setup/load-data.sh
@@ -1,17 +1,31 @@
 
-format=$1
-pool=$2
-objfile=$3
-nosds=$4
-nobjs=$5
+pool=$1
+objfile=$2
+nosds=$3
+nobjs=$4
 
+
+echo "pool=${pool}";
+echo "objfile=${objfile}";
+echo "nosds=${nosds}";
+echo "nobjs=${nobjs}";
+
+
+
+
+cd $HOME
 rados rmpool $pool $pool --yes-i-really-really-mean-it
 rados mkpool $pool
-groupsize=$((20*$nosds))
-echo $groupsize
+
+# magic number, seems work well at all data sizes 10MB/100MB and cluster scales 1,2,4
+groupsize_multiplier=20
+groupsize=$(($groupsize_multiplier*$nosds))
+echo "groupsize=${groupsize}";
 groupsize=$(( groupsize < nobjs ? groupsize : nobjs ))
-for ((i=0; i < $nobjs; i+=groupsize)); do
-    for ((j=0; j < groupsize; j++)); do
+echo "groupsize=${groupsize}";
+echo "nobjs=${nobjs}";
+for ((i=0; i<nobjs; i+=groupsize)); do
+    for ((j=0; j<groupsize; j++)); do
         oid=$(($i+$j))
         if [ "${oid}" -ge "${nobjs}" ]
         then
---
+++ src/progly/cloudlab_cluster_setup/load-wrapper-10MB.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+cd $HOME
+echo "Assumes data files are available at ${HOME}"
+echo "Usage:  ./this <numosds> <numobjs>"
+sleep 2s
+numosds=$1
+numobjs=$2
+pool_ncols100_10MB_fbx="ncols100.10MB.fbx"
+pool_ncols100_10MB_arrow="ncols100.10MB.arrow"
+pool_lineitem_10MB_fbx="lineitem.10MB.fbx"
+pool_lineitem_10MB_arrow="lineitem.10MB.arrow"
+echo `date`
+bash load-data.sh $pool_ncols100_10MB_fbx     fbx.ncols100.10MB.25Krows.obj.0  $numosds $numobjs;
+bash load-data.sh  $pool_ncols100_10MB_arrow arrow.ncols100.10MB   $numosds $numobjs;
+bash load-data.sh  $pool_lineitem_10MB_fbx     fbx.lineitem.10MB.75Krows.obj.0   $numosds $numobjs;
+bash load-data.sh  $pool_lineitem_10MB_arrow  arrow.lineitem.10MB.75Krows.obj.0  $numosds $numobjs;
+echo "loaded 10MB data into pools:"
+echo  $pool_ncols100_10MB_fbx
+echo  $pool_ncols100_10MB_arrow
+echo  $pool_lineitem_10MB_fbx
+echo $pool_lineitem_10MB_arrow
+echo `date`
+
+
+
+
+
