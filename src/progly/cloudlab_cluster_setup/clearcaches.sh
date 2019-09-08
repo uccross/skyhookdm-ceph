be6d28641bf3039aaa19a1ff0f4c8483b6cc1b5d
jlefevre <jlefevre@soe.ucsc.edu>
2019-09-07 22:09:44 -0700

    Script to clear cache on all osds.

---
+++ src/progly/cloudlab_cluster_setup/clearcaches.sh
@@ -0,0 +1,7 @@
+nosds=$1;
+for ((j=0; j<${nosds}; j++)); do
+    echo "clearing cache on osd"$j
+    ssh osd${j} sync
+    ssh osd${j} "echo 1 | sudo tee /proc/sys/vm/drop_caches"
+    ssh osd${j} sync
+done
