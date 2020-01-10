6f2a016565efa015ddf2d1aa357c9790440a899e
jeff <jlefevre@soe.ucsc.edu>
2020-01-10 14:22:55 -0600

    Updated install script for Ceph Luminous and Centos7 pkgs.

--- src/progly/cloudlab_cluster_setup/copy_ssh_keys.sh
+++ src/progly/cloudlab_cluster_setup/copy_ssh_keys.sh
@@ -1,21 +1,6 @@
 echo "assumes there is a local file 'nodes.txt' with the correct node names of all machines:clientX through osdX"
 echo "assumes this node has its ~/.ssh/id_rsa key present and permissions are 0600"
 echo "will copy ssh keys and known host signatures to the following nodes in 10 seconds:"
-
-FILE1="nodes.txt"
-if test -f "$FILE1"; then
-    echo "$FILE1 present, OK."
-else
-    echo "$FILE1 not present but required."
-    exit 1
-fi
-FILE2="${HOME}/.ssh/id_rsa"
-if test -f "$FILE2"; then
-    echo "$FILE2 present, OK."
-else
-    echo "$FILE2 not present but required."
-    exit 1
-fi
 cat nodes.txt
 sleep 10
 
