213600ab6d832227a481b837314537a5752bca96
akmontan <akmontan@ucsc.edu>
2020-04-02 01:08:34 -0500

    Minor changes for personal readability

--- src/progly/rados-store-glob.sh
+++ src/progly/rados-store-glob.sh
@@ -19,17 +19,23 @@ objects=($@)
 echo "Loading ${#objects[@]} into pool $pool..."
 echo ${objects[@]} | fold  -w 80 -s
 read -p "Continue? (y/n) " -n 1 -r
+
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  count=0
-  for ((i=0; i < ${#objects[@]}; i+=groupsize)); do
-    objgroup=("${objects[@]:i:groupsize}")
-    for objfile in ${objgroup[*]}; do
-      objname="${objprefix}.${tablename}.${count}"
-      rados -p $pool rm $objname || true
-      echo "writing $objfile into $pool/$objname"
-      rados -p $pool put $objname $objfile &
-      count=$((count+1))
+    count=0
+
+    for ((i=0; i < ${#objects[@]}; i+=groupsize)); do
+        objgroup=("${objects[@]:i:groupsize}")
+
+        for objfile in ${objgroup[*]}; do
+            objname="${objprefix}.${tablename}.${count}"
+
+            rados -p $pool rm $objname || true
+            echo "writing $objfile into $pool/$objname"
+            rados -p $pool put $objname $objfile &
+
+            count=$((count+1))
+        done
+
+        wait
     done
-    wait
-  done
 fi
