708bef64e6cb9792601a6bc3e93a4e8cce132ce7
Matthew Rhea <m.b.rhea31@gmail.com>
2020-05-19 03:20:45 +0000

    Quick typo fix

--- src/progly/sql_interface/unittest/test_simple.py
+++ src/progly/sql_interface/unittest/test_simple.py
@@ -2,8 +2,8 @@ import unittest
 import os 
 
 class TestSimple(unittest.TestCase): 
-    def test_a_projection:
+    def test_a_projection(self):
         return 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     unittest.main()
