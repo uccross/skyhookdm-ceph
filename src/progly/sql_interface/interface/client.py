405f2224bc118f4bae9efc07fb129b121d035b1e
Matthew Rhea <m.b.rhea31@gmail.com>
2020-06-03 13:23:37 -0700

    Add option to use SQL file as input to client.

--- src/progly/sql_interface/interface/client.py
+++ src/progly/sql_interface/interface/client.py
@@ -7,22 +7,31 @@ def changeSettings(input):
         return False
 
 def main(): 
-    # Use the same options for every query. TODO: Allow options changes while entering queries. 
-    print("Enter command options as command separated values belows. Press 'Enter' without input to use default options.")
+    print("Enter options as comma separated values belows. Press 'Enter' without input to use default options.")
     print("Options (must be in this order): [num_objs],[pool_name],[use-cls],[quiet]")
     print("Example: 2, tpchdata, [Y | N], [Y | N]")
     opts = input(">>> ")
 
     # Run until told otherwise.
     while(1):
-        print("Enter a SQL query (Multiple semi-colon separated queries can be accepted).")
-        print("Enter 'options' to change options.")
+        print("Enter a SQL query or SQL file (Multiple semi-colon separated queries can be accepted).")
+        print("Enter 'options' to change options. Enter 'file [file_1] ...' to use one or more SQL files.")
         rawUserQuery = input(">>> ")
 
         if changeSettings(rawUserQuery):
             print("Enter new options: ")
             opts = input(">>> ")
-            pass  
+            continue
+
+        if rawUserQuery.split()[0] == 'file': 
+            print(rawUserQuery)
+            for file in rawUserQuery.split(' ', 1)[1]: 
+                with open(file) as f: 
+                    # Max 10MB. TODO: Implement lazy method with 'yield' to read file piece by piece for large files
+                    size = 1024  
+                    read_queries = f.read(size)
+                handleQuery(opts, read_queries)
+            continue
 
         handleQuery(opts, rawUserQuery)
 
