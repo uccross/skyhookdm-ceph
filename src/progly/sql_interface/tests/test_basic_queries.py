1cfa3c5df56ede86902850ac4f550dfd2732140c
Matthew Rhea <m.b.rhea31@gmail.com>
2020-06-03 12:40:27 -0700

    Let users change options while running client

---
+++ src/progly/sql_interface/tests/test_basic_queries.py
@@ -0,0 +1,21 @@
+import unittest
+import os 
+from progly.sql_interface.interface import client  
+from progly.sql_interface.interface import parser
+
+class TestSimple(unittest.TestCase): 
+    def test_a_projection(self):
+        return 
+    
+    def test_b_selection(self): 
+        return 
+
+    def test_c_aggregation(self):
+        return 
+        
+    def test_d_cls_quiet_options(self): 
+        return 
+    
+
+if __name__ == '__main__':
+    unittest.main()
--- src/progly/sql_interface/tests/test_simple.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import os 
-
-class TestSimple(unittest.TestCase): 
-    def test_a_projection(self):
-        return 
-
-if __name__ == '__main__':
-    unittest.main()
