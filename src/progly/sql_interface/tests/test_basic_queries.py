fcaa738b97115b78de6f39f4626599919e722a18
Matthew Rhea <m.b.rhea31@gmail.com>
2020-06-19 15:33:54 -0700

    python modules/packages are annoying

--- src/progly/sql_interface/tests/test_basic_queries.py
+++ src/progly/sql_interface/tests/test_basic_queries.py
@@ -1,26 +1,16 @@
 import unittest
 import os 
-from interface import client  
-from interface import parser
+import sys
+from interface.client import prettyPrintHelp, prettyPrintIntro
 
 class TestBasicQueries(unittest.TestCase): 
     def test_a_projection(self):
+        prettyPrintHelp()
         return 
     
     def test_b_selection(self): 
+        prettyPrintIntro
         return 
-
-    def test_c_aggregation(self):
-        return 
-        
-    def test_d_cls_option(self): 
-        return 
-
-    def test_e_quiet_option(self):
-        return
-    
-    def test_f_all_options(self): 
-        return
 
 if __name__ == '__main__':
     unittest.main()
