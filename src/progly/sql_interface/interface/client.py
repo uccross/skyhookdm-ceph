d078dc9a8b79635e7592aa763b743a7e1503653f
Matthew Rhea <m.b.rhea31@gmail.com>
2020-06-18 20:30:22 -0700

    Better help message, cleaned up formatting, and working on changing options during execution

--- src/progly/sql_interface/interface/client.py
+++ src/progly/sql_interface/interface/client.py
@@ -1,67 +1,95 @@
+import re
+import sys
+import signal
 from parser import handleQuery 
 from optparse import OptionParser
 
-def changeSettings(input): 
-    if input == 'options':
-        return True
-    else: 
-        return False
+def changeOptions(opts):
+    possibleOptions = [
+        'num_objs',
+        'pool',
+        # 'cls' # TODO: Expose cls and quiet to users
+        # 'quiet'
+    ]
+    newOpts = input("\nWhat options do you want to change? " + str(possibleOptions) + "\n>>> ")
+    newOpts = re.split(', | ',newOpts)
+
+    for item in newOpts:
+        if item not in possibleOptions:
+            print("Invalid option: " + item + "\nReturning to SQL Client...\n")
+            break
+        val = input("What value do you want to change [" + item + "] to?\n>>> ")
+        opts[item] = val
+    print(opts)
+    return opts
 
 def prettyPrintHelp():
-    print("This is a help message")
+    print( # TODO: Expose \t--use-cls\n \t--quiet\n 
+'''
+Skyhook SQL Client Application\nOptions:\n \t-n [--num-objs]\n \t-p [--pool]" 
+
+Currently supported syntax:
+\t Projections (EXAMPLE: SELECT orderkey FROM lineitem)
+\t Selections  (EXAMPLE: SELECT orderkey FROM lineitem WHERE orderkey<3;)
+\t SHOW schema (EXAMPLE: DESCRIBE TABLE lineitem)
+
+To show this message enter:     'help'
+To quit this application enter: 'quit' 
+''')
+        
 
 def prettyPrintIntro():
     print('{:^100}'.format("Welcome to the Skyhook SQL Client Application")) 
     print('{:^100}'.format("---------------------------------------------"))
-    # print('{:>10}'.format("Here you can enter SQL queries such as: "))
-    # print('{:^50}'.format("\t\tSELECT * FROM lineitem; SELECT orderkey, tax, comment FROM lineitem\n"))
-    print('{:^100}'.format("(Enter 'help' for a brief summary of supported commands)"))
-    # print('{:^50}'.format("To change options enter 'options'; otherwise, if you need help enter 'help'.\nTo quit enter 'quit'"))
+    print('{:^100}'.format("(Enter 'help' for a brief summary of supported commands)\n"))
 
 def main():
     usage = "usage: python3 %prog [options]"
     optParser = OptionParser(usage)
     optParser.add_option("-c", "--use-cls", action="store_true", dest="cls", default=True, help="push execution onto storage servers using object classes")
     optParser.add_option("-q", "--quiet", action="store_true", dest="quiet", default=False, help="see summary of query results only")
-    optParser.add_option("-n", "--num-objs", default=2, help="number of storage objects.")
+    optParser.add_option("-n", "--num-objs", default=2, help="number of storage objects.") 
     optParser.add_option("-p", "--pool", default="tpchdata", help="name of object pool")
-
     (options, args) = optParser.parse_args()
-    print(options)
-    print(args)
 
     prettyPrintIntro()
 
-    print("Enter a SQL query (Multiple semi-colon separated queries can be accepted).")
+    print("Enter a SQL query (multiple semi-colon separated queries can be accepted).")
     # print("Enter 'options' to change options. Enter 'file [file_1] ...' to use one or more SQL files.") # TODO: Formatting for changings options and using SQL files
     
     # Run until told otherwise.
-    while(1):
-        rawUserQuery = input(">>> ")
+    while True:
+        try: 
+            rawUserQuery = input(">>> ")
 
-        if changeSettings(rawUserQuery):
-            print("Enter new options: ")
-            opts = input(">>> ")
+            opts = vars(options)
+            # TODO: Implement pythonic switch-case using dicts for checking input? 
+            if rawUserQuery == 'options': # TODO: Expose option changing to user
+                print("Current options: ", end=' ')
+                print(opts)
+                opts = changeOptions(opts)
+                continue
+            if rawUserQuery == 'help':
+                prettyPrintHelp()
+                continue
+            if rawUserQuery == 'quit':
+                break
+
+            if rawUserQuery.split()[0] == 'file': 
+                print(rawUserQuery)
+                for file in rawUserQuery.split(' ', 1)[1].split():
+                    print(rawUserQuery.split(' ', 1)[1])
+                    print(file)
+                    with open(file) as f: 
+                        # Max 10MB. TODO: Implement lazy method with 'yield' to read file piece by piece for large files
+                        size = 1024  
+                        read_queries = f.read(size)
+                    handleQuery(opts, read_queries)
+                continue
+
+            handleQuery(opts, rawUserQuery)
+        except:
             continue
-
-        if rawUserQuery == 'help':
-            prettyPrintHelp()
-            continue
-        opts = vars(options)
-        if rawUserQuery.split()[0] == 'file': 
-            print(rawUserQuery)
-            for file in rawUserQuery.split(' ', 1)[1].split():
-                print(rawUserQuery.split(' ', 1)[1])
-                print(file)
-                with open(file) as f: 
-                    # Max 10MB. TODO: Implement lazy method with 'yield' to read file piece by piece for large files
-                    size = 1024  
-                    read_queries = f.read(size)
-                handleQuery(opts, read_queries)
-            continue
-
-        handleQuery(opts, rawUserQuery)
-
 
 if __name__ == "__main__":
     main()
