f6f8d972dfc09df55042961b3900fbdf66906964
Matthew Rhea <m.b.rhea31@gmail.com>
2020-06-26 15:06:48 -0700

    Fix variable scoping

--- src/progly/sql_interface/tests/test_basic_queries.py
+++ src/progly/sql_interface/tests/test_basic_queries.py
@@ -7,47 +7,66 @@ import sys
 from interface.translator import handleQuery
 
 class TestBasicQueries(unittest.TestCase): 
-    defaultOpts = {'use-cls': True, 
-                   'quiet': False, 
-                   'num-objs': 2, 
-                   'pool': 'tpchdata'}
-
-    def test_a_projection_1(self):
+    def test_a_projection_1_def(self):
+        defaultOpts = {'use-cls': True, 
+                'quiet': False, 
+                'num-objs': 2, 
+                'pool': 'tpchdata'}
         query = "SELECT * \
                  FROM lineitem"
         handleQuery(defaultOpts, query)
 
-    def test_b_projection_2(self):
+    def test_b_projection_2_def(self):
+        defaultOpts = {'use-cls': True, 
+                'quiet': False, 
+                'num-objs': 2, 
+                'pool': 'tpchdata'}
         query = "SELECT orderkey,tax,linenumber,returnflag \
             FROM lineitem"
         handleQuery(defaultOpts, query)
 
-    def test_c_projection_3(self):
+    def test_c_projection_3_quiet(self):
+        opts = {'use-cls': False, 
+                'quiet': True, 
+                'num-objs': 2, 
+                'pool': 'tpchdata'}
         query = "SELECT linenumber,returnflag \
                  FROM lineitem"
-        handleQuery(defaultOpts, query)
+        handleQuery(opts, query)
 
-    def test_d_projection_4(self):
+    def test_d_projection_4_no_quiet_no_cls(self):
+        opts = {'use-cls': False, 
+                    'quiet': False, 
+                    'num-objs': 2, 
+                    'pool': 'tpchdata'}
         query = "SELECT shipdate, commitdate \
                  FROM lineitem"
-        handleQuery(defaultOpts, query)
+        handleQuery(opts, query)
 
-    def test_e_selection_1(self): 
+    def test_e_selection_1_def(self): 
+        opts = {'use-cls': True, 
+                    'quiet': False, 
+                    'num-objs': 2, 
+                    'pool': 'tpchdata'}
         query = "SELECT orderkey \
                  FROM lineitem \
                  WHERE orderkey > 3"
-        handleQuery(defaultOpts, query) 
+        handleQuery(opts, query) 
 
-    def test_f_selection_2(self):
+    def test_f_selection_2_quiet(self):
+        opts = {'use-cls': True, 
+                    'quiet': True, 
+                    'num-objs': 2, 
+                    'pool': 'tpchdata'}
         query = "SELECT orderkey,tax,comment,lienumber,returnflag \
                  FROM lineitem \
                  WHERE orderkey <> 2"
-        handleQuery(defaultOpts, query)
+        handleQuery(opts, query)
 
         query = "SELECT orderkey,tax,comment,lienumber,returnflag \
                  FROM lineitem \
                  WHERE orderkey != 2"
-        handleQuery(defaultOpts, query)
+        handleQuery(opts, query)
 
 if __name__ == '__main__':
     unittest.main()
